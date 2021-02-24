fb8439cd4d607bcb8b47c73d30e3eb78e15b0337
Evgeny Epifanov <workeev@gmail.com>
2021-02-24 09:42:58 +0300

    settings

---
+++ app/src/main/java/ru/eecode/dir/ui/prefs/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package ru.eecode.dir.ui.prefs
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import ru.eecode.dir.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
